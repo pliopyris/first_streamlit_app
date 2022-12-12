9754f6ed74514a17e5490ca26158d2d61e6e0bcd
Pantelis Liopyris <112958193+pliopyris@users.noreply.github.com>
2022-12-12 18:43:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Fruit Smoothie')
+streamlit.text('Hard-boiled egg')
