9de2ad6b82c913a42e95491532d91e00186cefff
Pantelis Liopyris <112958193+pliopyris@users.noreply.github.com>
2022-12-12 18:45:39 +0200

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-# streamlit.header('Breakfast Menu')
-# streamlit.text('Omega3 & Blueberry Oatmeal')
-# streamlit.text('Fruit Smoothie')
-# streamlit.text('Hard-boiled egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Fruit Smoothie')
+streamlit.text('Hard-boiled egg')
