e0e15d3041964c15fe55c85cc955b310594d9f0e
Pantelis Liopyris <112958193+pliopyris@users.noreply.github.com>
2022-12-14 14:21:17 +0200

    Update streamlit_app.py
    read user choice and pass it as parameter to the api request

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,11 @@ st.dataframe(fruits_to_show)
 # Display FruityVice API response
 st.header('Fruityvice Fruit Advice')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# user choice
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # st.text(fruityvice_response.json())
 
 # normalize api response (json) and pass it to a df
